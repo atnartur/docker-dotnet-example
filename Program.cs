e79e46a10eb24c875464b377a5af3ae81698f9f5
atnartur <i@atnartur.ru>
2018-03-28 00:29:09 +0300

    + npgsql

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 
 namespace docker_dotnet_example
 {
@@ -14,6 +15,17 @@ namespace docker_dotnet_example
     {
         public static void Main(string[] args)
         {
+            var connString = "Host=db;Username=test;Password=test;Database=test";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("SELECT datname FROM pg_database WHERE datistemplate = false", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0));
+            }
+
             BuildWebHost(args).Run();
         }
 
